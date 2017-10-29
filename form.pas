7ad0141591f79c7288c3d6422700988a78410ec2
Andrey Markov <markov@programmer.net>
2017-10-29 23:51:11 +0400

    Сконструирована форма.

--- form.pas
+++ form.pas
@@ -5,10 +5,29 @@ unit Form;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus,
+  ComCtrls, Grids, StdCtrls, Types;
 
 type
-  TForm1 = class(TForm)
+
+  { TDatabase }
+
+  TDatabase = class(TForm)
+    Table2Edit: TEdit;
+    Table2StringGrid: TStringGrid;
+    Table1Button: TButton;
+    Table1Edit: TEdit;
+    MainMenu: TMainMenu;
+    DB: TMenuItem;
+    dbCreate: TMenuItem;
+    dbOpen: TMenuItem;
+    dbSave: TMenuItem;
+    dbClose: TMenuItem;
+    PageControl: TPageControl;
+    Table1StringGrid: TStringGrid;
+    Table1TabSheet: TTabSheet;
+    Table2TabSheet: TTabSheet;
+    Table2Button: TToggleBox;
   private
     { private declarations }
   public
@@ -16,11 +35,13 @@ type
   end;
 
 var
-  Form1: TForm1;
+  Database: TDatabase;
 
 implementation
 
 {$R *.lfm}
+
+{ TDatabase }
 
 end.
 
